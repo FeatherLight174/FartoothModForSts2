a0f5585aed4ad67c8398791efe1c87a7eb0759a1
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2026-05-27 12:40:21 +0800

    distance power completed

--- Relics/Sniper.cs
+++ Relics/Sniper.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fartooth.Powers;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
-
+using MegaCrit.Sts2.Core.Models.Events;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Rooms;
 namespace Fartooth.Relics;
 
 public sealed class Sniper : RelicModel
@@ -17,21 +22,23 @@ public sealed class Sniper : RelicModel
     public override RelicRarity Rarity => RelicRarity.Starter;
     // 稀有度
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new EnergyVar(2) // 关联 能量 的动态变量
-        ];
-    // 动态变量
 
     public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
     {
-        // 判断事件调用时是否为遗物持有者一方，且回合数是否为 1
-        if (side == Owner.Creature.Side && combatState.RoundNumber == 1)
+        // 判断事件调用时是否为遗物持有者一方
+        if (side == Owner.Creature.Side)
         {
+            await Cmd.CustomScaledWait(0.2f, 0.4f);
             Flash(); // 触发遗物图标闪烁
-            await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
-            // 给予玩家能量
+           
+            await PowerCmd.Apply<Distance>(Owner.Creature, 1m, Owner.Creature, null);
+
+
         }
     }
+    
+
+
 }
 
-    
+
